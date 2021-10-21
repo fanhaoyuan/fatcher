15ce854a4d0947d1d5a3fdef3601fba935abcac8
fanhaoyuan <418536538@qq.com>
2021-10-21 23:57:07 +0800

    feat: add `shouldMiddlewareApply`

--- src/core/middlewares.ts
+++ src/core/middlewares.ts
@@ -1,6 +1,19 @@
-import { Middleware } from '../interfaces';
+import { Middleware, Immutable, RequestContext } from '../interfaces';
 import { uuid, isFunction } from '../utils';
 
+/**
+ * Return boolean for should middleware apply with context.
+ * @param middleware
+ * @param context
+ * @returns
+ */
+export function shouldMiddlewareApply(middleware: Middleware, context: Immutable<RequestContext>) {
+    if (isFunction(middleware.apply)) {
+        return middleware.apply(context) ?? true;
+    }
+
+    return middleware.apply ?? true;
+}
 /**
  * Register Middlewares of fetch.
  *
