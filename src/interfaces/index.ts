b2d6ab258b0eb4f6ed2586bda117f7dc6e914c43
fanhaoyuan <418536538@qq.com>
2021-10-21 23:48:13 +0800

    types: add types of `Middleware`

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -19,3 +19,70 @@ export type RequestMethod =
 
 export type ImmutableObject<T extends Record<string, any>> = { readonly [K in keyof T]: Immutable<T[K]> };
 export type Immutable<T> = T extends Record<string, any> ? ImmutableObject<T> : T;
+
+export interface Middleware {
+    /**
+     * Middleware name
+     */
+    name: string;
+
+    /**
+     * Should middleware apply.
+     *
+     * If `false`, middlewares will filter in register middleware
+     *
+     * If `true`, middleware will always run in request.
+     *
+     * If `function`, middleware will run with return true or void.
+     *
+     * @default true
+     */
+    apply?: ((context: Immutable<RequestContext>) => boolean) | boolean;
+
+    /**
+     * Middleware function.
+     */
+    use(context: Immutable<RequestContext>, next: MiddlewareNext): Promise<ResponseType> | ResponseType;
+}
+
+export interface RequestOptions extends RequestInit {
+    /**
+     * The prefix url with http request.
+     *
+     * It will splice into the front of url.
+     */
+    baseURL?: string;
+
+    /**
+     * URL to request.
+     */
+    url?: string;
+
+    /**
+     * Middleware to register.
+     */
+    middlewares?: ((() => Middleware) | Middleware)[];
+    payload?: Record<string, any>;
+    /**
+     * Request headers.
+     */
+    headers?: Record<string, string>;
+
+    /**
+     * Request Method
+     */
+    method?: RequestMethod;
+}
+
+export interface RequestContext {
+    /**
+     * Request Options
+     */
+    options: RequestOptions;
+}
+
+export type ResponseType = Response | ArrayBuffer | FormData | string | ReadableStream<Uint8Array>;
+
+export interface MiddlewareNext {
+    (context?: RequestContext): Promise<ResponseType> | ResponseType;
+}
