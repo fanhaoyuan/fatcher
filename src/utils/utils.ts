7ed2a61b57f01e24981a86658f5652d36f7e5e4d
fanhaoyuan <418536538@qq.com>
2021-10-22 00:08:53 +0800

    feat: add tools function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,14 @@
+export function uuid(length = 6) {
+    const STRING_TEMPLATE = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    const coefficient = STRING_TEMPLATE.length;
+
+    let string = '';
+
+    for (let i = 0; i < length; i++) {
+        string += STRING_TEMPLATE.charAt(Math.floor(Math.random() * coefficient));
+    }
+    return string;
+}
 /**
  * Judgment whether a variable is function
  * @param f var
@@ -5,4 +16,8 @@
  */
 export function isFunction(f: unknown): f is Function {
     return typeof f === 'function';
+}
+
+export function isString(s: unknown): s is string {
+    return typeof s === 'string';
 }
