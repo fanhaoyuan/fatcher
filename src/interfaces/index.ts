675b9c351ed1382b93dfd1aa4868e04e1d3b7093
fanhaoyuan <418536538@qq.com>
2021-10-21 23:38:12 +0800

    types: add `RequestMethod`

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,21 @@
 /* eslint-disable no-use-before-define */
+export type RequestMethod =
+    | 'get'
+    | 'post'
+    | 'delete'
+    | 'put'
+    | 'head'
+    | 'options'
+    | 'patch'
+    | 'connect'
+    | 'GET'
+    | 'POST'
+    | 'DELETE'
+    | 'PUT'
+    | 'HEAD'
+    | 'OPTIONS'
+    | 'PATCH'
+    | 'CONNECT';
+
 export type ImmutableObject<T extends Record<string, any>> = { readonly [K in keyof T]: Immutable<T[K]> };
 export type Immutable<T> = T extends Record<string, any> ? ImmutableObject<T> : T;
