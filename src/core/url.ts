adca537a69b90531893e195fd1c7c21b5817615f
fanhaoyuan <418536538@qq.com>
2021-10-25 21:36:13 +0800

    feat: add `stringify` and `parse`

---
+++ src/core/url.ts
@@ -0,0 +1,60 @@
+/**
+ * Transform an object to a query string.
+ * @param object
+ * @returns
+ */
+export function stringify<T extends Record<string, any>>(object: T) {
+    return Object.keys(object)
+        .reduce<string[]>((array, key) => {
+            let k, v;
+
+            try {
+                k = encodeURIComponent(key);
+                v = encodeURIComponent(object[key] ?? '');
+            } catch {
+                return array;
+            }
+
+            return array.concat([`${k}=${v}`]);
+        }, [])
+        .join('&');
+}
+
+/**
+ * Parse a query string to a plain object
+ * @param url
+ * @returns
+ */
+export function parse(url: string) {
+    const object: Record<string, any> = {};
+
+    if (!url) {
+        return object;
+    }
+
+    const matched = url.match(/([^=?#&]+)=?([^&]*)/g);
+
+    if (!matched) {
+        return object;
+    }
+
+    for (const match of matched) {
+        let [key, value] = match.split('=');
+
+        try {
+            key = decodeURIComponent(key);
+        } catch {
+            continue;
+        }
+
+        try {
+            value = decodeURIComponent(value);
+        } catch {
+            continue;
+        }
+
+        object[key] = value ?? '';
+    }
+
+    return object;
+}
