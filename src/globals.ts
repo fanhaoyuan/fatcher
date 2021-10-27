964f326ebd8197027dda5ad8be979483e8fda44a
fanhaoyuan <418536538@qq.com>
2021-10-27 17:30:44 +0800

    refactor: use `globals options` instead.

---
+++ src/globals.ts
@@ -0,0 +1,22 @@
+import { RequestOptions } from './interfaces';
+
+/**
+ * Global request options.
+ *
+ * Compose order is `inline options -> local options -> global options`
+ */
+export const globalOptions: RequestOptions = {
+    baseURL: '/',
+    url: '',
+    payload: {},
+    method: 'get',
+    headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    withCredentials: 'auto',
+    autoTransformPayload: true,
+    middlewares: [],
+    signal: null,
+    onAbort: null,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export * from './middlewares';
+import { fatch } from './fatch';
+export * from './globals';
+export { aborter } from './middlewares';
 export * from './interfaces';
 export { isAbort } from './helpers';
+export { fatch };
