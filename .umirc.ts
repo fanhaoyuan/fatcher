738927c52209d62666a3353e55f1f485e4018b1d
Fansy <418536538@qq.com>
2021-11-12 23:15:48 +0800

    chore: add `dumi` for docs

---
+++ .umirc.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+    title: 'Fatcher',
+    mode: 'site',
+    outputPath: 'docs-dist',
+    // more config: https://d.umijs.org/config
+});
