d2e4fe585566a65f9c32156de3e95782ea3a6d68
好运 <1282527500@qq.com>
2021-11-13 15:29:15 +0800

    feat: add demo and readable file

--- sandbox/server.js
+++ sandbox/server.js
@@ -1,24 +1,39 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const BodyParser = require('koa-bodyparser');
 const cors = require('koa2-cors');
 const fs = require('fs');
 const app = new Koa();
 const router = new Router();
+const bodyParser = new BodyParser();
+
+router.use(bodyParser);
 
 let getInfo = fs.readFileSync('./static/get.json').toString();
 let postInfo = fs.readFileSync('./static/post.json').toString();
 
-router.get('/check/getCheckInfo', (ctx, next) => {
+router.get('/zcs/check/getCheckInfo', (ctx, next) => {
     ctx.set('Content-Type', 'application/json');
     ctx.set('Access-Control-Allow-Origin', '*');
-    ctx.body = getInfo;
+    ctx.body = {
+        data: getInfo,
+        code: 200,
+        success: true,
+    };
     next();
 });
 
-router.post('/group/groupList', (ctx, next) => {
+router.post('/zcs/group/groupList', (ctx, next) => {
     ctx.set('Content-Type', 'application/json');
     ctx.set('Access-Control-Allow-Origin', '*');
-    ctx.body = postInfo;
+    ctx.body = {
+        data: postInfo,
+        code: 200,
+        success: true,
+    };
+    console.log('------------------------------------------------------');
+    console.log(ctx.request);
+    console.log(ctx.request.body);
     next();
 });
 
