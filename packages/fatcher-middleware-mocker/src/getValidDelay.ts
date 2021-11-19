15a9a55b5490928f1fcfb942dccaa7477a25c3a0
Fansy <35361626+fanhaoyuan@users.noreply.github.com>
2021-11-20 00:20:03 +0800

    feat: add range for delay

--- packages/fatcher-middleware-mocker/src/getValidDelay.ts
+++ packages/fatcher-middleware-mocker/src/getValidDelay.ts
@@ -12,10 +12,18 @@ export function getValidDelay(delay: number | number[]) {
             return min;
         }
 
-        min = Math.min(min, max);
-        max = Math.max(min, max);
+        min = Math.min(min, max, 0);
+        max = Math.max(min, max, 1);
 
         return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    if (delay > 1) {
+        return 1;
+    }
+
+    if (delay < 0) {
+        return delay;
     }
 
     return delay;
