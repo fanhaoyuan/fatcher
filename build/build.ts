069f8222a27e006c19cee34c630faea49e6a7af7
fanhaoyuan <418536538@qq.com>
2021-11-22 11:46:19 +0800

    build: use rollup.config.js instead.

--- build/build.ts
+++
@@ -1,3 +0,0 @@
-import * as builder from './builder';
-
-builder.builder();
---
+++ packages/fatcher/rollup.config.js
@@ -0,0 +1,88 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import esbuild from 'rollup-plugin-esbuild';
+import { main, module as esm, browser } from './package.json';
+import { terser } from 'rollup-plugin-terser';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import rm from 'rimraf';
+import path from 'path';
+
+function getCurrentPath(...absolutePath) {
+    return path.resolve(__dirname, ...absolutePath);
+}
+
+rm.sync(getCurrentPath('dist'));
+
+const input = getCurrentPath('src/index.ts');
+
+/**
+ * @type {import('rollup').RollupOptions[]}
+ */
+const config = [
+    {
+        input,
+        plugins: [nodeResolve(), commonjs(), esbuild({ target: 'esnext' })],
+        output: [
+            {
+                format: 'es',
+                file: getCurrentPath(esm),
+                plugins: [
+                    getBabelOutputPlugin({
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    targets: {
+                                        esmodules: true,
+                                    },
+                                },
+                            ],
+                        ],
+                    }),
+                ],
+            },
+            {
+                format: 'cjs',
+                file: getCurrentPath(main),
+                plugins: [
+                    getBabelOutputPlugin({
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    targets: {
+                                        esmodules: true,
+                                    },
+                                },
+                            ],
+                        ],
+                    }),
+                ],
+            },
+            {
+                format: 'umd',
+                file: getCurrentPath(browser),
+                name: 'Fatcher',
+                plugins: [
+                    getBabelOutputPlugin({
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    modules: 'umd',
+                                    targets: {
+                                        esmodules: true,
+                                    },
+                                },
+                            ],
+                        ],
+                        allowAllFormats: true,
+                    }),
+                    process.env.NODE_ENV === 'production' ? terser() : false,
+                ],
+            },
+        ],
+    },
+];
+
+export default config;
