34e45afbe3e0166014ce22f3d31df72c6c2253bb
fanhaoyuan <418536538@qq.com>
2021-11-22 18:21:27 +0800

    types: update return types

--- packages/fatcher/src/helpers/isFatcherError.ts
+++ packages/fatcher/src/helpers/isFatcherError.ts
@@ -5,7 +5,7 @@ import { FatcherError } from '../core/FatcherError';
  * @param error
  * @returns
  */
-export function isFatcherError(error: FatcherError) {
+export function isFatcherError(error: FatcherError): error is FatcherError {
     try {
         return error instanceof FatcherError && error.name === 'FatcherError' && error.isFatcherError;
     } catch {
