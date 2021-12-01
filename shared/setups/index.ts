03703b27da8db9bd041874f48a34f234353e119b
fanhaoyuan <418536538@qq.com>
2021-12-01 11:35:04 +0800

    test: update test env

--- shared/setups/index.ts
+++ shared/setups/index.ts
@@ -1,5 +1,7 @@
 import fetchMock from 'jest-fetch-mock';
+import errors from './errors';
 import methods from './methods';
+import mock from './mock';
 import { BASE_URL } from './utils';
 
 fetchMock.mockIf(/^https:\/\/fatcher\.mock\.com\/.*/, async request => {
@@ -7,6 +9,7 @@ fetchMock.mockIf(/^https:\/\/fatcher\.mock\.com\/.*/, async request => {
 
     if (url.startsWith('/errors/')) return errors(request);
     if (url.startsWith('/methods/')) return methods(request);
+    if (url.startsWith('/mock/')) return mock(request);
 
     return {
         status: 404,
