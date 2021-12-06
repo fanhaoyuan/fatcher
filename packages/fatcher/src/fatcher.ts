e4ea296ca8d8f9ae56091034c630ef0e02e47cd8
fanhaoyuan <418536538@qq.com>
2021-12-07 00:38:27 +0800

    chore: use initial options

--- packages/fatcher/src/fatcher.ts
+++ packages/fatcher/src/fatcher.ts
@@ -9,17 +9,17 @@ import { isString } from 'utils-shared';
 import { isFatcherError } from './helpers';
 
 export async function fatcher<T = any>(payload: string, options?: Partial<RequestOptions>): Promise<Response<T>>;
-export async function fatcher<T = any>(payload?: Partial<RequestOptions> & { url: string }): Promise<Response<T>>;
+export async function fatcher<T = any>(payload: Partial<RequestOptions> & { url: string }): Promise<Response<T>>;
 export async function fatcher(
-    payload?: (Partial<RequestOptions> & { url: string }) | string,
-    inlineOptions?: Partial<RequestOptions>
+    payload: (Partial<RequestOptions> & { url: string }) | string,
+    inlineOptions: Partial<RequestOptions> = {}
 ) {
     let mergedOptions: RequestOptions;
 
     if (isString(payload)) {
-        mergedOptions = mergeOptions(globalOptions, inlineOptions ?? {}, { url: payload });
+        mergedOptions = mergeOptions(globalOptions, inlineOptions, { url: payload });
     } else {
-        mergedOptions = mergeOptions(globalOptions, payload ?? {});
+        mergedOptions = mergeOptions(globalOptions, payload);
     }
 
     const middlewares = [responseFormatter, ...mergedOptions.middlewares, payloadTransformer, fetch];
