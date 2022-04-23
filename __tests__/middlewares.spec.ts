059ac0f015b2d0340d4c482d0a7020be19ce5295
fanhaoyuan <418536538@qq.com>
2022-04-23 16:39:40 +0800

    test: add `middlewares` test case

---
+++ __tests__/middlewares.spec.ts
@@ -0,0 +1,140 @@
+import { fatcher, Middleware, isFatcherError, canActivate, chunkStreamReader } from '../src';
+import fetchMock from 'jest-fetch-mock';
+import { BASE_URL } from './utils';
+
+describe('Custom Middlewares', () => {
+    const TEXT_LENGTH = 1_000_000;
+
+    let longText = '';
+
+    beforeEach(() => {
+        fetchMock.mockIf(new RegExp(`${BASE_URL}/.*`), async request => {
+            if (request.url === `${BASE_URL}/getLongText`) {
+                if (request.method !== 'POST') {
+                    return {
+                        status: 405,
+                    };
+                }
+
+                while (longText.length < TEXT_LENGTH) {
+                    longText += Math.random().toString(36).slice(-5);
+                }
+
+                return {
+                    status: 200,
+                    body: longText,
+                };
+            }
+
+            return {
+                status: 404,
+            };
+        });
+
+        fetchMock.enableMocks();
+    });
+
+    function pre(): Middleware {
+        return {
+            name: 'fatcher-middleware-pre',
+            use(context, next) {
+                if (context.url === '/get') {
+                    return next({
+                        url: '/getLongText',
+                        method: 'POST',
+                    });
+                }
+
+                return next();
+            },
+        };
+    }
+
+    it("Don't use pre middleware", async () => {
+        try {
+            await fatcher({
+                baseUrl: BASE_URL,
+                url: '/get',
+            });
+        } catch (error: any) {
+            expect(isFatcherError(error)).toBe(true);
+
+            const json = error.toJSON();
+
+            expect(json.status).toBe(404);
+        }
+    });
+
+    it('Using pre middleware', async () => {
+        const { status, data } = await fatcher({
+            baseUrl: BASE_URL,
+            url: '/get',
+            middlewares: [pre()],
+        });
+
+        expect(status).toBe(200);
+        expect(data.toString()).toBe(longText);
+    });
+
+    function post(cof: number): Middleware {
+        let index = 0;
+
+        return {
+            name: 'fatcher-middleware-post',
+            async use(context, next) {
+                const { data: response, ...rest } = await next();
+
+                if (!canActivate(response)) {
+                    return {
+                        data: response,
+                        ...rest,
+                    };
+                }
+
+                const text = await response.text();
+
+                const stream = new ReadableStream({
+                    start(controller) {
+                        (function push() {
+                            const currentText = text.slice(index * cof, (index + 1) * cof);
+
+                            if (!currentText) {
+                                controller.close();
+                                return;
+                            }
+
+                            index++;
+
+                            controller.enqueue(currentText);
+                            push();
+                        })();
+                    },
+                });
+
+                return {
+                    ...rest,
+                    response,
+                    data: stream,
+                };
+            },
+        };
+    }
+
+    it('Compose Middlewares', async () => {
+        const COF = 1000;
+
+        const { data } = await fatcher<ReadableStream<string>>({
+            baseUrl: BASE_URL,
+            url: '/get',
+            middlewares: [pre(), post(COF)],
+        });
+
+        const result: string[] = [];
+        await chunkStreamReader(data, str => result.push(str));
+        expect(result[0].length).toBe(COF);
+        expect(result.length).toBe(longText.length / COF);
+        expect(result.join('')).toBe(longText);
+    });
+
+    afterEach(() => fetchMock.disableMocks());
+});
