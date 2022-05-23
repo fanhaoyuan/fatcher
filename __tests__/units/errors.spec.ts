8ac93e85adcecdbc0bc27b0d9e54136f22afd76e
fanhaoyuan <418536538@qq.com>
2022-05-23 11:00:19 +0800

    test: add `errors` units tests

---
+++ __tests__/units/errors.spec.ts
@@ -0,0 +1,38 @@
+import { Context } from 'fatcher';
+import { FatcherError, isFatcherError } from 'fatcher/errors';
+
+describe('Errors', () => {
+    let fatcherError: FatcherError;
+
+    const context: Context = {
+        url: '/errors/test',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const responseBody = {
+        userId: 'test',
+        userName: 'fatcher',
+    };
+
+    const response = new Response(JSON.stringify(responseBody), {
+        status: 404,
+        headers: context.headers as Record<string, string>,
+    });
+
+    it('FatcherError', () => {
+        fatcherError = new FatcherError(context, response);
+
+        const json = fatcherError.toJSON();
+
+        expect(json.context).toStrictEqual(context);
+        expect(json.status).toBe(404);
+        expect(json.headers['content-type']).toBe(context.headers!['Content-Type']);
+    });
+
+    it('isFatcherError', () => {
+        expect(isFatcherError(fatcherError)).toBe(true);
+        expect(isFatcherError(new Error())).toBe(false);
+    });
+});
