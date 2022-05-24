0784e8fd2a476d091a65fed5e7bd864ab6551753
fanhaoyuan <418536538@qq.com>
2022-05-24 09:55:03 +0800

    test: add `utils` units tests

---
+++ __tests__/units/utils.spec.ts
@@ -0,0 +1,60 @@
+import { immutable, isAbsoluteURL, isFunction, normalizeURL, unreachable } from 'fatcher/utils';
+
+describe('Utils', () => {
+    it('isAbsoluteURL', () => {
+        expect(isAbsoluteURL('https://a/test')).toBe(true);
+        expect(isAbsoluteURL('a/b/c')).toBe(false);
+        expect(isAbsoluteURL('http://a/b/c')).toBe(true);
+        expect(isAbsoluteURL('/test/a')).toBe(false);
+    });
+
+    it('normalizeURL', () => {
+        expect(normalizeURL('/test', '/a')).toBe('/test/a');
+        expect(normalizeURL('https://test/a/c', '/t/e')).toBe('https://test/a/c/t/e');
+        expect(normalizeURL('/', '')).toBe('/');
+        expect(normalizeURL('/', 'https://a/c')).toBe('https://a/c');
+        expect(normalizeURL('/', '/a/c')).toBe('/a/c');
+    });
+
+    it('isFunction', () => {
+        expect(isFunction(() => null)).toBe(true);
+        expect(isFunction('1')).toBe(false);
+        expect(isFunction(2)).toBe(false);
+        expect(isFunction({})).toBe(false);
+        expect(isFunction([])).toBe(false);
+        expect(isFunction(null)).toBe(false);
+        expect(isFunction(new Map())).toBe(false);
+        expect(isFunction(new Set())).toBe(false);
+        // eslint-disable-next-line no-new-func
+        expect(isFunction(new Function())).toBe(true);
+    });
+
+    it('immutable', () => {
+        const object = {
+            a: 'c',
+        };
+
+        const immutableObject = immutable(object);
+
+        try {
+            immutableObject.a = '';
+        } catch (error) {
+            expect(error instanceof TypeError).toBe(true);
+        }
+
+        expect(immutableObject.a).toBe('c');
+
+        object.a = '';
+
+        expect(object.a).toBe('');
+    });
+
+    it('unreachable', () => {
+        try {
+            unreachable('test');
+        } catch (error: any) {
+            expect(error instanceof Error).toBe(true);
+            expect(error.message).toBe('[Fatcher] test');
+        }
+    });
+});
