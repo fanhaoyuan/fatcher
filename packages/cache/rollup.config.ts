59d2fce62e3d5dc7da83fd6aba6f96e2206398ca
Fansy <35361626+fanhaoyuan@users.noreply.github.com>
2022-06-06 18:23:47 +0800

    feat: add `cache` into packages (#139)

---
+++ packages/cache/rollup.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from 'rollup';
+import dts from 'rollup-plugin-dts';
+import esbuild, { minify } from 'rollup-plugin-esbuild';
+import nodeResolver from '@rollup/plugin-node-resolve';
+import { main, module, browser, typings } from './package.json';
+
+const input = 'src/index.ts';
+const plugins = [nodeResolver(), esbuild({ target: 'es2018' })];
+
+export default defineConfig([
+    {
+        input,
+        plugins,
+        external: ['fatcher'],
+        output: [
+            {
+                format: 'cjs',
+                file: main,
+            },
+            {
+                format: 'module',
+                file: module,
+            },
+            {
+                plugins: [minify()],
+                format: 'umd',
+                file: browser,
+                name: 'FatcherMiddlewareCache',
+                globals: {
+                    fatcher: 'Fatcher',
+                },
+            },
+        ],
+    },
+    {
+        input,
+        plugins: [dts()],
+        output: {
+            format: 'esm',
+            file: typings,
+        },
+    },
+]);
