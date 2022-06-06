4bca41ada2610c6572e30a82545249a2b097839e
Fansy <35361626+fanhaoyuan@users.noreply.github.com>
2022-06-06 17:49:06 +0800

    feat: add `progress` into packages (#136)

---
+++ packages/progress/README.md
@@ -0,0 +1,58 @@
+# @fatcherjs/middleware-progress
+
+A Middleware for getting progress
+
+## Install
+
+### NPM
+
+```bash
+>$ npm install @fatcherjs/middleware-progress
+```
+
+### CDN
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/@fatcherjs/middleware-progress/dist/index.min.js"></script>
+```
+
+## Usage
+
+```ts
+import { progress } from '@fatcherjs/middleware-progress';
+import { fatcher } from 'fatcher';
+
+fatcher({
+    url: '/bar/foo',
+    middlewares: [
+        progress({
+            onDownloadProgress: (current, total) => {
+                console.log(current / total);
+            },
+        }),
+    ],
+    payload: {
+        bar: 'foo',
+    },
+})
+    .then(res => {
+        console.log(res);
+    })
+    .catch(err => {
+        console.error(error);
+    });
+```
+
+## Options
+
+### onDownloadProgress
+
+-   Type: `((current: number, total: number) => void) | null`
+-   DefaultValue: `null`
+-   Description:
+
+Callback with read stream chunks.
+
+## License
+
+[MIT](../../LICENSE)
---
+++ packages/progress/src/progress.ts
@@ -0,0 +1,40 @@
+import { Middleware, readStreamByChunk } from 'fatcher';
+import { ProgressOptions } from './interfaces';
+
+/**
+ * A Middleware for getting progress
+ * @param options
+ * @returns
+ */
+export function progress(options: ProgressOptions = {}): Middleware {
+    const { onDownloadProgress = null } = options;
+
+    return {
+        name: 'fatcher-middleware-progress',
+        async use(context, next) {
+            const result = await next();
+
+            if (!onDownloadProgress) {
+                return result;
+            }
+
+            const total = ~~(result.headers.get('content-length') || 0);
+
+            if (total === 0) {
+                return result;
+            }
+
+            let current = 0;
+
+            const clonedResponse = result.data.clone();
+
+            readStreamByChunk(clonedResponse.body, chunk => {
+                current += chunk.length;
+
+                onDownloadProgress?.(current, total);
+            });
+
+            return result;
+        },
+    };
+}
