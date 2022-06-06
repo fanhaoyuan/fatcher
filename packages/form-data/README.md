75db29f2894bc79a3b8211634f6f1bd3dba1f893
fanhaoyuan <418536538@qq.com>
2022-06-06 17:33:41 +0800

    feat: add `form-data` into packages

---
+++ packages/form-data/README.md
@@ -0,0 +1,45 @@
+# @fatcherjs/middleware-form-data
+
+A middleware for consuming payload to form data
+
+## Install
+
+### NPM
+
+```bash
+>$ npm install @fatcherjs/middleware-form-data
+```
+
+### CDN
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/@fatcherjs/middleware-form-data/dist/index.min.js"></script>
+```
+
+## Usage
+
+```ts
+import { formData } from '@fatcherjs/middleware-form-data';
+import { fatcher } from 'fatcher';
+
+fatcher({
+    url: '/bar/foo',
+    middlewares: [formData()],
+    payload: {
+        bar: 'foo',
+    },
+    headers: {
+        'Content-Type': 'multipart/form-data',
+    },
+})
+    .then(res => {
+        console.log(res);
+    })
+    .catch(err => {
+        console.error(error);
+    });
+```
+
+## License
+
+[LICENSE](../../LICENSE)
---
+++ packages/form-data/src/formData.ts
@@ -0,0 +1,56 @@
+import { Middleware } from 'fatcher';
+
+/**
+ * A middleware for consuming payload to form data.
+ * @returns
+ */
+export function formData(): Middleware {
+    return {
+        name: 'fatcher-middleware-form-data',
+        async use(context, next) {
+            const { headers = {}, payload = null } = context;
+
+            const contentType = headers['Content-Type'];
+
+            if (!contentType?.includes('multipart/form-data')) {
+                return next();
+            }
+
+            const originType = contentType;
+
+            let body: FormData | null = null;
+
+            if (payload) {
+                body = new FormData();
+
+                for (const key of Object.keys(payload)) {
+                    const value = payload[key];
+
+                    if (Array.isArray(value)) {
+                        value.forEach(item => body?.append(key, item));
+                        continue;
+                    }
+
+                    body.append(key, value);
+                }
+            }
+
+            const response = await next({
+                payload: null,
+                body,
+                headers: {
+                    ...headers,
+                    'Content-Type': null,
+                },
+            });
+
+            return {
+                ...response,
+                headers: {
+                    ...response.headers,
+                    'Content-Type': originType,
+                },
+            };
+        },
+    };
+}
