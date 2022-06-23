3ebbb55951f4253401337c59cb3471ee883b96d5
fanhaoyuan <418536538@qq.com>
2022-06-23 16:50:10 +0800

    docs(vite.config.ts): remove useless config

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
-const scopedNameMap = new Map();
-
 export default async () => {
     const mdx = (await import('@mdx-js/rollup')).default;
     const frontmatter = (await import('remark-frontmatter')).default;
@@ -27,22 +25,6 @@ export default async () => {
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, 'src'),
-            },
-        },
-        css: {
-            modules: {
-                localsConvention: 'camelCaseOnly',
-                generateScopedName(name, fileName) {
-                    if (!scopedNameMap.has(fileName)) {
-                        scopedNameMap.set(fileName, Math.random().toString(36).slice(-6));
-                    }
-
-                    if (/^prismjs.*/.test(name)) {
-                        return name;
-                    }
-
-                    return `${name}__${scopedNameMap.get(fileName)}`;
-                },
             },
         },
         optimizeDeps: {
