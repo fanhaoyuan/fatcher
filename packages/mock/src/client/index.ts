873e71944f4c47cd80d68e8c00ed4ac801c3f77b
fanhaoyuan <418536538@qq.com>
2022-08-02 17:41:25 +0800

    feat: update mocker

--- packages/mock/src/client/index.ts
+++ packages/mock/src/client/index.ts
@@ -1,2 +1,3 @@
 export * from './interfaces';
 export * from './mock';
+export * from './register';
--- packages/mock/src/node/index.ts
+++ packages/mock/src/node/index.ts
@@ -1,13 +1,2 @@
-// import { fatcher } from 'fatcher';
-// import { json } from '@fatcherjs/middleware-json';
-// import * as fs from 'fs';
-
-// fatcher({
-//     url: 'https://petstore.swagger.io/v2/swagger.json',
-//     middlewares: [json()],
-// }).then(res => {
-//     fs.writeFileSync('./swagger.json', JSON.stringify(res.data, null, 2), 'utf-8');
-// });
-
 export * from './generator';
 export * from './resolveConfig';
