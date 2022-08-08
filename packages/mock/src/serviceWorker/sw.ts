750b42d97356fe53d1a7824ea0270774161420cc
fanhaoyuan <418536538@qq.com>
2022-08-08 10:00:34 +0800

    feat(mock): move `ResponseStatusMap` to utils

--- packages/mock/src/serviceWorker/sw.ts
+++ packages/mock/src/serviceWorker/sw.ts
@@ -2,7 +2,7 @@
 import Schema from 'async-validator';
 import { MockConfig } from '../interfaces';
 import { parser } from '../parser';
-import { MOCK_HEADER_KEY } from '../utils';
+import { MOCK_HEADER_KEY, getResponseStatus } from '../utils';
 import { paramsValidator } from '../validator';
 
 declare let self: ServiceWorkerGlobalScope;
@@ -17,58 +17,6 @@ self.addEventListener('activate', async () => {
 
     clients.forEach(client => client.postMessage([client.id, 'activated']));
 });
-
-const responseMap = {
-    100: 'Continue',
-    101: 'Switching Protocols',
-    200: 'OK',
-    201: 'Created',
-    202: 'Accepted',
-    203: 'Non-Authoritative Information',
-    204: 'No Content',
-    205: 'Reset Content',
-    206: 'Partial Content',
-    300: 'Multiple Choice',
-    301: 'Moved Permanently',
-    302: 'Found',
-    303: 'See Other',
-    304: 'Not Modified',
-    305: 'Use Proxy',
-    307: 'Temporary Redirect',
-    400: 'Bad Request',
-    401: 'Unauthorized',
-    402: 'Payment Required',
-    403: 'Forbidden',
-    404: 'Not Found',
-    405: 'Method Not Allowed',
-    406: 'Not Acceptable',
-    407: 'Proxy Authentication Required',
-    408: 'Request Timeout',
-    409: 'Conflict',
-    410: 'Gone',
-    411: 'Length Required',
-    412: 'Precondition Failed',
-    413: 'Request Entity Too Large',
-    414: 'Request-URI Too Long',
-    415: 'Unsupported Media Type',
-    416: 'Requested Range Not Satisfiable',
-    417: 'Expectation Failed',
-    422: 'Unprocessable Entity',
-    500: 'Internal Server Error',
-    501: 'Not Implemented',
-    502: 'Bad Gateway',
-    503: 'Service Unavailable',
-    504: 'Gateway Timeout',
-    505: 'HTTP Version Not Supported',
-};
-
-function responseInit(status: keyof typeof responseMap, init: ResponseInit = {}) {
-    return {
-        status,
-        statusText: responseMap[status],
-        ...init,
-    };
-}
 
 /** Inject Mock Schema Code */
 
@@ -101,7 +49,7 @@ self.addEventListener('fetch', async event => {
                  * If Method is not match. Response with 405.
                  */
                 if (request.method !== method) {
-                    event.respondWith(new Response(null, responseInit(400)));
+                    event.respondWith(new Response(null, getResponseStatus(405)));
                     return;
                 }
 
@@ -112,7 +60,7 @@ self.addEventListener('fetch', async event => {
                     try {
                         await paramsValidator(querystring, params);
                     } catch (error) {
-                        event.respondWith(new Response(null, responseInit(400)));
+                        event.respondWith(new Response(null, getResponseStatus(400)));
                         return;
                     }
                 }
@@ -138,7 +86,7 @@ self.addEventListener('fetch', async event => {
                     try {
                         await new Schema(body).validate(requestBody);
                     } catch (error) {
-                        event.respondWith(new Response(null, { status: 400, statusText: 'Bad Request' }));
+                        event.respondWith(new Response(null, getResponseStatus(400)));
                         return;
                     }
                 }
@@ -155,14 +103,12 @@ self.addEventListener('fetch', async event => {
             }
 
             event.respondWith(
-                new Response(
-                    JSON.stringify(data),
-                    responseInit(200, {
-                        headers: {
-                            'content-type': 'application/json',
-                        },
-                    })
-                )
+                new Response(JSON.stringify(data), {
+                    ...getResponseStatus(200),
+                    headers: {
+                        'content-type': 'application/json',
+                    },
+                })
             );
             return;
         }
--- packages/mock/src/utils/index.ts
+++ packages/mock/src/utils/index.ts
@@ -1,3 +1,17 @@
 export const MOCK_HEADER_KEY = 'x-fatcher-mock';
 
 export const isNodeJS = typeof process !== 'undefined' && process.versions && process.versions.node;
+
+export const ResponseStatusMap = {
+    200: 'OK',
+    400: 'Bad Request',
+    405: 'Method Not Allowed',
+    500: 'Internal Server Error',
+};
+
+export function getResponseStatus(status: keyof typeof ResponseStatusMap) {
+    return {
+        status,
+        statusText: ResponseStatusMap[status],
+    };
+}
