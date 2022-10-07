f486c61a84b56dd5453ef908fd6874a3c96d9bf6
Fansy <418536538@qq.com>
2022-10-07 12:57:32 +0800

    refactor: flatten helpers into `helpers.ts`

--- packages/fatcher/src/helpers.ts
+++ packages/fatcher/src/helpers.ts
@@ -1,4 +1,13 @@
-import { MaybePromise } from '../interfaces';
+import { MaybePromise } from './interfaces';
+
+/**
+ * Confirm a data can be transform.
+ * @param response
+ * @returns
+ */
+export function canActivate(data: unknown): data is Response {
+    return data instanceof Response && !data.bodyUsed && !!data.body;
+}
 
 /**
  * Read a readable stream by chunk
--- packages/fatcher/src/helpers/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './canActivate';
-export * from './readStreamByChunk';
