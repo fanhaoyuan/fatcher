410bf7633c827f279167a15324da9af82d569219
Fansy <418536538@qq.com>
2022-10-18 00:28:07 +0800

    refactor: remove `fatcherjs/utils-shared`

--- packages/utils-shared/__tests__/isNodeJS.spec.ts
+++
@@ -1,7 +0,0 @@
-import { isNodeJS } from '../src';
-
-describe('isNodeJS', () => {
-    it('Test env is in node js', () => {
-        expect(isNodeJS()).toBe(true);
-    });
-});
--- packages/utils-shared/src/isFunction.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Confirm a value whether is a function
- * @param value value to confirm
- * @returns
- */
-export function isFunction(value: unknown): value is (...args: any[]) => any {
-    return typeof value === 'function';
-}
