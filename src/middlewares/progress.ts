eff93e12f3ff679b5f3bfcfa43e3502615248ea9
Fansy <418536538@qq.com>
2023-02-19 14:53:54 +0800

    refactor: move dir with `middleware-progress`

--- src/middlewares/progress.ts
+++ src/middlewares/progress.ts
@@ -1,7 +1,22 @@
-import { canActivate } from '../../core';
-import { defineMiddleware } from '../../helpers';
-import { Middleware } from '../../interfaces';
-import { ProgressOptions } from './interfaces';
+import { canActivate } from '../core';
+import { defineMiddleware } from '../helpers';
+import { Middleware } from '../interfaces';
+
+export interface ProgressOptions {
+    /**
+     * Callback with read stream chunks.
+     *
+     * @default null
+     */
+    onDownloadProgress?: ((current: number, total: number) => void) | null;
+
+    /**
+     * Custom name in headers with content-length
+     *
+     * @default 'content-length'
+     */
+    lengthName?: string;
+}
 
 /**
  * A Middleware for getting progress
