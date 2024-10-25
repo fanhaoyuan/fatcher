b07bd759518dd7554f125b6ec0f6276a7ed77e5b
fanhaoyuan <418536538@qq.com>
2024-10-25 20:28:02 +0800

    types: add readStreamByChunk types

--- packages/utils-shared/src/readStreamByChunk.ts
+++ packages/utils-shared/src/readStreamByChunk.ts
@@ -1,8 +1,8 @@
-export async function readStreamByChunk(
-  readableStream: ReadableStream,
-  callback: (value: string) => Promise<void> | void,
+export async function readStreamByChunk<T = Uint8Array>(
+  readableStream: ReadableStream<T>,
+  callback: (value: T) => Promise<void> | void,
 ) {
-  async function read(reader: ReadableStreamDefaultReader) {
+  async function read(reader: ReadableStreamDefaultReader<T>) {
     const { value, done } = await reader.read();
     if (done) {
       return;
