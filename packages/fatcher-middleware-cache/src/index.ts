554597be4eed3263814c5007f46828d62b7c0c13
Fansy <418536538@qq.com>
2024-11-01 16:35:06 +0800

    feat: update middleware cache

--- packages/fatcher-middleware-cache/src/index.ts
+++ packages/fatcher-middleware-cache/src/index.ts
@@ -1,2 +1,2 @@
-export * from './middleware';
+export * from './cache';
 export * from './types';
--- packages/fatcher-middleware-cache/src/types.ts
+++ packages/fatcher-middleware-cache/src/types.ts
@@ -1,4 +1,19 @@
-export interface CacheOptions {
-  ttl?: number;
-  useCache?: boolean;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type * as fatcher from 'fatcher';
+
+declare module 'fatcher' {
+  interface FatcherOptions {
+    /**
+     * Cache a Response for a while
+     * Will not cache when ttl is `0`
+     * @default 0
+     */
+    ttl?: number;
+
+    /**
+     * Ignore cache and send request when is `true`
+     * @default false
+     */
+    flush?: boolean;
+  }
 }
